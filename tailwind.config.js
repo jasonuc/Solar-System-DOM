2485f764bd50a38d231ac88d36e79205da35d6b0
jasonuc <mrjasonu@gmail.com>
2023-10-26 00:04:12 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./build/scripts/*.js",
+    "./build/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
