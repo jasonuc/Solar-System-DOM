3d32e7bd6a728250511c56bc8d5f240cad83b5d0
jasonuc <mrjasonu@gmail.com>
2023-10-26 15:07:02 +0100

    adds planets object and more to js

--- build/scripts/main.js
+++ build/scripts/main.js
@@ -1,2 +1,22 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+const weightInput = document.querySelector('.weight-input');
+const planetNameOptn = document.querySelector('.planet-name-optn');
+const calcBtn = document.querySelector('.calc-btn');
+const planetName = document.querySelector('.planet-name');
+const planetImg = document.querySelector('.planet-image');
+const weightOnPlanet = document.querySelector('.weight-on-planet');
+
+const planets = {
+    none: 0,
+    mercury: 3.7,
+    venus: 8.87,
+    earth: 9.8,
+    mars: 3.71,
+    jupiter: 24.79,
+    saturn: 10.44,
+    uranus: 8.69,
+    neptune: 11.15,
+    pluto: 0.62 
+};
+
+console.log(weightOnPlanet);
+console.log(planets);
