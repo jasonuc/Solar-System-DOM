47b20e08811948e801f841270a95c4bb00fb1cc2
jasonuc <mrjasonu@gmail.com>
2023-10-26 11:22:12 +0100

    adds changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,29 @@ module.exports = {
     "./build/*.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'frank-ruhl-libre': ['Frank Ruhl Libre', 'ui-serif'],
+        'lato': ['Lato', 'ui-sans-serif'],
+        'libre-display': ['Libre Caslon Display', 'ui-serif'],
+        'libre-text': ['Libre Caslon Text', 'ui-serif'],
+        'oswald': ['Oswald', 'ui-sans-serif'],
+        'roboto-slab': ['Roboto Slab', 'ui-serif']
+      },
+      backgroundImage: {
+        'galaxy-gif': "url('../images/galaxy.gif')",
+        'earth': "url('../images/earth.png')",
+        'jupiter': "url('../images/jupiter.png')",
+        'mars': "url('../images/mars.png')",
+        'mercury': "url('../images/mercury.png')",
+        'moon': "url('../images/moon.png')",
+        'neptune': "url('../images/neptune.png')",
+        'pluto': "url('../images/pluto.png')",
+        'saturn': "url('../images/saturn.png')",
+        'uranus': "url('../images/uranus.png')",
+        'venus': "url('../images/venus.png')"
+      }
+    },
   },
   plugins: [],
 }
-
